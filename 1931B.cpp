3bb5b378f3dc18f60e121ef2ebf58c41a333ac36
Argus817 <147311119+Argus817@users.noreply.github.com>
2024-02-28 11:03:23 +0530

    Update 1931B.cpp

--- 1931B.cpp
+++ 1931B.cpp
@@ -3,36 +3,23 @@ using namespace std;
 
 int makeEqual()
 {
-    int n, max=0;
+    int n, max=0, sum=0, temp=0;
     cin >> n;
     int arr[n];
     for (int i=0; i<n; i++)
-        cin >> arr[i];
-        
-    if (n==1)
-        return 1;
-    for (int i=n-1; i>=0; i--)
-    {    
-        if (arr[i] > max)
-        {
-            int sum=0;
-            for (int j=i; j<n; j++)
-                sum += arr[j];
-            int rem = sum%(n-i);
-            for (int j=i; j<=n; j++)
-            {
-                arr[j] = sum/(n-i);
-                if (rem>0)
-                    arr[j]++;
-            }
-        }
-        max = (max>arr[i])? max:arr[i];
-    }
-    for (int i=1; i<n; i++)
     {
-        if (arr[i]!=arr[i-1])
-            return 0;
+        cin >> arr[i];
+        sum+=arr[i];
     }
+    int k = sum/n;
+    for (int i=0; i<n; i++)
+    {
+        arr[i] += temp;
+        temp=0;
+        if (arr[i]>=k)
+            temp+=arr[i]-k;
+        else return 0;
+    }    
     return 1;
 }
 
